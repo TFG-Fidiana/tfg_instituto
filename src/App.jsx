1eee6370a8ff034f69794ef39149d7c1606cf4fd
Samuel Plaza Sáez <samuelplazasaez@gmail.com>
2025-04-04 10:42:57 +0200

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Login from './components/Login'
+import "./css/App.css";
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+      {/* <div className='container'>
+        
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
+
